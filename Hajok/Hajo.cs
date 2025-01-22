f3472f72868017bcc2549cef62ca1080e48a65fc
Tibi81 <kisss.tibi@gmail.com>
2025-01-22 19:35:48 +0100

    Add project files.

---
+++ Hajok/Hajo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Hajok
+{
+    internal class Hajo
+    {
+        public string Nev { get; set; }
+        public string Tipus { get; set; }
+        public int Gyartas_eve { get; set; }
+        public int Uzemel { get; set; }
+        public string Tulajdonos { get; set; }
+        public string Varos { get; set; }
+
+        public Hajo(string sor)
+        {
+            string[] resz = sor.Split(';');
+            Nev = resz[0];
+            Tipus = resz[1];
+            Gyartas_eve = int.Parse(resz[2]);
+            Uzemel = int.Parse(resz[3]);    
+            Tulajdonos = resz[4];
+            Varos = resz[5];
+        }
+
+        public bool Kiemelt()
+        {
+            if (Gyartas_eve < 1990)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+    }
+}
---
+++ Hajok/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Hajok
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Hajo> adatok = new List<Hajo>();
+            foreach (var i in File.ReadAllLines("hajolista.csv"))
+            { 
+                Hajo x = new Hajo(i);
+                adatok.Add(x);                
+            }
+            Console.WriteLine("4. feladat: Hajók száma: " + adatok.Count);
+
+            foreach (var i in adatok)
+            {
+                if (i.Tipus == "személyhajó" & i.Varos != "Siófok")
+                {
+                    Console.WriteLine("5. feladat: Siófokon kívüli hajók:" + i.Nev);
+                    Console.WriteLine(i.Kiemelt());
+                    Console.WriteLine(i.Gyartas_eve);
+
+                }
+            }
+            Console.Write("7. feladat: Kérem a város nevét: ");
+
+            string varos = Console.ReadLine();
+
+            int osszeg = 0;
+            int db = 0;
+
+            foreach (var i in adatok)
+            {
+                if (i.Varos == varos)
+                {
+                    osszeg += i.Gyartas_eve;
+                    db++;
+                }
+            }
+
+            if (db == 0)
+            {
+                Console.WriteLine("Nem található hajó a megadott városban.");
+            }
+            else
+            {
+                Console.WriteLine($"A hajók átlagos gyártási éve:  {osszeg / db }");
+            }            
+            Console.ReadKey();
+        }
+    }
+}
